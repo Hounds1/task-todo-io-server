9f19a333c20b0443799fe1eeb12ecdf538ac0414
Hounds1 <zoodisx@gmail.com>
2023-04-15 14:02:50 +0900

    git init

---
+++ src/main/java/task/todo/io/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package task.todo.io.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
